fe330f76c0e8f29d29e9be5769add81817a378bd
Jose Carlos De Moraes Filho <jose.filho@sistemafiep.org.br>
2021-03-15 12:41:30 -0300

    feat: First version

---
+++ README.md
@@ -0,0 +1,12 @@
+## Purpose
+This repository was made with the intention of learning how to use canvas and proof a concept about region selection on images.
+
+## Refs
+
+* https://yushulx.medium.com/a-simple-web-canvas-region-selection-tool-for-barcode-region-detection-69cb3b9e6348
+* https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Hit_regions_and_accessibility
+* https://developer.mozilla.org/pt-BR/docs/Web/API/Canvas_API/Tutorial/Optimizing_canvas
+* https://www.w3schools.com/tags/canvas_clip.asp
+
+## Demo
+![App Demonstration](./demo.gif)
---
+++ main.js
@@ -0,0 +1,179 @@
+window.onload = () => {
+  const imageMaxLength = 500;
+  DOM.$elements["canvas"] = document.getElementById("imageCanvas");
+  DOM.$elements["overlay"] = document.getElementById("overlay");
+  DOM.$elements["cursorSection"] = document.getElementById("mousePosition");
+  DOM.$elements["tagInputSection"] = document.getElementById("tag-section");
+  DOM.$elements["imageInput"] = document.getElementById("image-input");
+  DOM.$elements["addTagButton"] = document.getElementById("btn-add-tag");
+  DOM.$elements["inputTag"] = document.getElementById("input-classify");
+  DOM.$elements["toggleResultTriggers"] = document.querySelectorAll(
+    ".toggle-classifier-results"
+  );
+  DOM.$elements["classifierSection"] = document.getElementById(
+    "classifier-section"
+  );
+  DOM.$elements["classifiersResult"] = document.getElementById(
+    "classifiers-result"
+  );
+  Canvas.$canvas = DOM.$elements.canvas;
+  Canvas.$overlay = DOM.$elements.overlay;
+  Canvas.canvasContext = Canvas.$canvas.getContext("2d");
+  Canvas.overlayContext = Canvas.$overlay.getContext("2d");
+
+  DOM.$elements.toggleResultTriggers.forEach(
+    Listeners.onToggleClassifierResults
+  );
+  Listeners.onImageChange(imageMaxLength);
+};
+
+const DOM = {
+  $elements: {},
+};
+
+const Classifiers = {
+  current: {},
+  list: [],
+};
+
+const Listeners = {
+  DOMElements: DOM.$elements,
+  onToggleClassifierResults($element) {
+    $element.addEventListener("click", (e) => {
+      if (
+        Listeners.DOMElements.classifierSection.classList.contains("hidden")
+      ) {
+        Listeners.DOMElements.classifierSection.classList.remove("hidden");
+      } else {
+        Listeners.DOMElements.classifierSection.classList.add("hidden");
+      }
+    });
+  },
+  onImageChange(maxLength) {
+    Listeners.DOMElements.imageInput.addEventListener("change", (e) => {
+      if (Listeners.DOMElements.imageInput.files.length == 0) {
+        return;
+      }
+
+      let img = new Image();
+
+      var reader = new FileReader();
+      reader.onload = function (evt) {
+        img.onload = function () {
+          if (img.width > maxLength) {
+            img.height = (maxLength * img.height) / img.width;
+            img.width = maxLength;
+          } else if (img.height > maxLength) {
+            img.width = (maxLength * img.width) / img.height;
+            img.height = maxLength;
+          }
+          Canvas.$canvas.width = img.width;
+          Canvas.$canvas.height = img.height;
+          Canvas.$overlay.width = Canvas.$canvas.width;
+          Canvas.$overlay.height = Canvas.$canvas.height;
+
+          Canvas.canvasContext.drawImage(img, 0, 0, img.width, img.height);
+        };
+        img.src = evt.target.result;
+
+        Listeners.onOverlayMouseDown();
+        Listeners.onOverlayMouseMove();
+        Listeners.onOverlayMouseUp();
+        Listeners.onAddTagButtonClick();
+      };
+      reader.readAsDataURL(Listeners.DOMElements.imageInput.files[0]);
+    });
+  },
+  onOverlayMouseDown() {
+    Listeners.DOMElements.overlay.addEventListener("mousedown", (e) => {
+      Canvas.initialPoint.x = e.offsetX;
+      Canvas.initialPoint.y = e.offsetY;
+      Canvas.isDrawing = true;
+      Canvas.clearOverlay();
+      Canvas.$overlay.style.cursor = "crosshair";
+    });
+  },
+  onOverlayMouseMove() {
+    Listeners.DOMElements.overlay.addEventListener("mousemove", (e) => {
+      if (Canvas.isDrawing) {
+        Canvas.drawReactangle(e);
+      }
+      Listeners.DOMElements.cursorSection.innerHTML =
+        "Mouse Cursor: (" + e.offsetX + ", " + e.offsetY + ")";
+    });
+  },
+  onOverlayMouseUp() {
+    Listeners.DOMElements.overlay.addEventListener("mouseup", (e) => {
+      if (Canvas.isDrawing) {
+        Canvas.isDrawing = false;
+
+        Listeners.DOMElements.cursorSection.innerHTML =
+          "Mouse Cursor: (" + e.offsetX + ", " + e.offsetY + ")";
+        Canvas.$overlay.style.cursor = "default";
+        Canvas.finalPoint.x = e.offsetX;
+        Canvas.finalPoint.y = e.offsetY;
+        Classifiers.current = {
+          topLeft: { x: Canvas.initialPoint.x, y: Canvas.initialPoint.y },
+          bottomLeft: { x: Canvas.initialPoint.x, y: Canvas.finalPoint.y },
+          bottomRight: { x: Canvas.finalPoint.x, y: Canvas.finalPoint.y },
+          topRight: { x: Canvas.finalPoint.x, y: Canvas.initialPoint.y },
+        };
+        Listeners.DOMElements.tagInputSection.classList.remove("hidden");
+      }
+    });
+  },
+  onAddTagButtonClick() {
+    Listeners.DOMElements.addTagButton.addEventListener("click", (e) => {
+      Classifiers.current = {
+        ...Classifiers.current,
+        tag: Listeners.DOMElements.inputTag.value,
+      };
+      Classifiers.list.push(Classifiers.current);
+      Listeners.DOMElements.classifiersResult.innerHTML = JSON.stringify(
+        Classifiers.list,
+        null,
+        "\t"
+      );
+      Canvas.clearOverlay();
+      Listeners.DOMElements.inputTag.value = "";
+      Listeners.DOMElements.tagInputSection.classList.add("hidden");
+    });
+  },
+};
+
+const Canvas = {
+  $canvas: null,
+  $overlay: null,
+  canvasContext: null,
+  overlayContext: null,
+  initialPoint: {
+    x: 0,
+    y: 0,
+  },
+  finalPoint: {
+    x: 0,
+    y: 0,
+  },
+  isDrawing: false,
+  clearOverlay() {
+    Canvas.overlayContext.clearRect(
+      0,
+      0,
+      Canvas.$overlay.width,
+      Canvas.$overlay.height
+    );
+    Canvas.overlayContext.strokeStyle = "#ff0000";
+    Canvas.overlayContext.lineWidth = 2;
+  },
+  drawReactangle(event) {
+    Canvas.clearOverlay();
+    Canvas.overlayContext.beginPath();
+    Canvas.overlayContext.rect(
+      Canvas.initialPoint.x,
+      Canvas.initialPoint.y,
+      event.offsetX - Canvas.initialPoint.x,
+      event.offsetY - Canvas.initialPoint.y
+    );
+    Canvas.overlayContext.stroke();
+  },
+};
